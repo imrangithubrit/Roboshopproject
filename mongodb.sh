c3c9f281324a27fa3c1aa3f0254b79b45b3e978b
imrangithubrit <devopsandaws2023@gmail.com>
2023-11-21 15:49:46 +0530

    m

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,14 @@
-
+ 
+ echo -e "\e[33m Copy Mongodb repo file \e[0m"
 # need to copy repo file
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-yum install mongodb-org -y 
+echo -e "\e[33m Installing Mongodb server \e[0m"
+yum install mongodb-org -y  &>>/tmp/roboshop.log
 
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl enable mongod 
 
-
-
+echo -e "\e[33m start mongodb \e[0m"
+systemctl enable mongod  &>>/tmp/roboshop.log
 systemctl restart mongod

4b3f8906ea127d8faadaeebfb465950a6311ac23
imrangithubrit <devopsandaws2023@gmail.com>
2023-11-21 14:03:10 +0530

    name chane

--- Frontend.sh
+++ Frontend.sh
@@ -1,20 +1,19 @@
 
 echo -e "\e[33m Installing the nginx server \e[0m"
-yum install nginx -y  >/tmp/roboshop.log
+yum install nginx -y  >/tmp/roboshopssss.log
 
 echo -e "\e[33m Installing old app content \e[0m"
-rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
-
+rm -rf /usr/share/nginx/html/*  >/tmp/roboshopssss.log
 echo -e "\e[33m Downloading the frontend contend \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshopssss.log 
 
 echo -e "\e[33m Extract frontend content \e[0m"
 cd /usr/share/nginx/html   
-unzip /tmp/frontend.zip  >/tmp/roboshop.log
+unzip /tmp/frontend.zip  >/tmp/roboshopssss.log
 
 
 # we need to add Roboshop config file
 echo -e "\e[33m Starting Nginx server \e[0m"
-systemctl enable nginx  >/tmp/roboshop.log
-systemctl restart nginx  >/tmp/roboshop.log
+systemctl enable nginx  >/tmp/roboshopssss.log
+systemctl restart nginx  >/tmp/roboshopssss.log
 

4d5f4338f9c97c6fe7e2a3e1fd2902a5cd8a0ce1
imrangithubrit <devopsandaws2023@gmail.com>
2023-11-23 22:22:18 +0530

    sd

--- cart.sh
+++ cart.sh
@@ -1,26 +1,34 @@
 
+echo -e "\e[33m Disable nodejs \e[0m"  
+yum module disable nodejs -y  &>>/tmp/roboshop.log
+echo -e "\e[33m nable nodejs \e[0m"
+yum module enable nodejs:18 -y  &>>/tmp/roboshop.log
 
-yum module disable nodejs -y
-yum module enable nodejs:18 -y
+echo -e "\e[33m install nodejs \e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
 
-yum install nodejs -y
+echo -e "\e[33m add user \e[0m"
+useradd roboshop &>>/tmp/roboshop.log
 
+echo -e "\e[33m create directory \e[0m"
+mkdir /app  &>>/tmp/roboshop.log
 
-useradd roboshop
-
-
-mkdir /app 
-
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
+echo -e "\e[33m Download application content \e[0m"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log 
 cd /app 
-unzip /tmp/cart.zip
 
+echo -e "\e[33m unzip the folder \e[0m"
+unzip /tmp/cart.zip  &>>/tmp/roboshop.log
+
+echo -e "\e[33m dependencies \e[0m"
 cd /app 
-npm install 
+npm install &>>/tmp/roboshop.log
 
-cp /Project/Roboshopproject/cart.service /etc/systemd/system/cart.service
+echo -e "\e[33m copy service file \e[0m"
+cp /Project/Roboshopproject/cart.service /etc/systemd/system/cart.service  &>>/tmp/roboshop.log
 
-systemctl daemon-reload
-
-systemctl enable cart 
-systemctl restart cart
+echo -e "\e[33m start cart \e[0m"
+systemctl daemon-reload  &>>/tmp/roboshop.log
+ 
+systemctl enable cart &>>/tmp/roboshop.log
+systemctl restart cart &>>/tmp/roboshop.log

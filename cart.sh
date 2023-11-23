b573da8219615c2bf5a91cf528141c6eeeea889a
imrangithubrit <devopsandaws2023@gmail.com>
2023-11-23 21:02:19 +0530

    sds

---
+++ cart.sh
@@ -0,0 +1,26 @@
+
+
+yum module disable nodejs -y
+yum module enable nodejs:18 -y
+
+yum install nodejs -y
+
+
+useradd roboshop
+
+
+mkdir /app 
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
+cd /app 
+unzip /tmp/cart.zip
+
+cd /app 
+npm install 
+
+cp /Project/Roboshopproject/cart.service /etc/systemd/system/cart.service
+
+systemctl daemon-reload
+
+systemctl enable cart 
+systemctl restart cart

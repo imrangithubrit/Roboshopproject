d74a41c8678fe96c6539fcc6c9fffe7c581a6069
imrangithubrit <devopsandaws2023@gmail.com>
2023-11-28 13:06:32 +0530

    scs

---
+++ shipping.sh
@@ -0,0 +1,30 @@
+yum install maven -y
+
+useradd roboshop
+
+rm -rf /app
+
+mkdir /app 
+
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
+cd /app 
+unzip /tmp/shipping.zip
+
+
+cd /app 
+mvn clean package 
+mv target/shipping-1.0.jar shipping.jar 
+
+
+systemctl daemon-reload
+
+systemctl enable shipping 
+systemctl restart shipping
+
+yum install mysql -y 
+
+
+mysql -h mysql-dev.devopsb72.store	 -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+
+systemctl restart shipping

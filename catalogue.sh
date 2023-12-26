01d5a74ffa2a190c1c7226c3f419799661f70fa7
imrangithubrit <devopsandaws2023@gmail.com>
2023-12-27 00:17:03 +0530

    xcd

--- catalogue.sh
+++ catalogue.sh
@@ -14,25 +14,25 @@ mkdir /app
 
 echo -e "\e[33m Download application director \e[0m"
 
-curl -o /tmp/component.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip  &>>/tmp/roboshop.log
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Extract application content \e[0m"
 cd /app   &>>/tmp/roboshop.log
-unzip /tmp/component.zip  &>>/tmp/roboshop.log
+unzip /tmp/$component.zip  &>>/tmp/roboshop.log
 cd /app 
 
 echo -e "\e[33m Install nodejs sependencies \e[0m"
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[33m setup component service \e[0m"
+echo -e "\e[33m setup $component service \e[0m"
 # cp service
-cp /project/Roboshopproject/component.service /etc/systemd/system/component.service  &>>/tmp/roboshop.log
+cp /project/Roboshopproject/$component.service /etc/systemd/system/$component.service  &>>/tmp/roboshop.log
 
-echo -e "\e[33m  start component service \e[0m"
+echo -e "\e[33m  start $component service \e[0m"
 systemctl daemon-reload  &>>/tmp/roboshop.log
 
-systemctl enable component &>>/tmp/roboshop.log
-systemctl restart component  &>>/tmp/roboshop.log
+systemctl enable $component &>>/tmp/roboshop.log
+systemctl restart $component  &>>/tmp/roboshop.log
 
 echo -e "\e[33m copy mongodb repo file \e[0m"
 #copy mpng.rep
@@ -41,4 +41,4 @@ cp /project/Roboshopproject/mongo.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/robo
 echo -e "\e[33m install mongodb \e[0m"
 yum install mongodb-org-shell -y   &>>/tmp/roboshop.log
 
-mongo --host mongodb-dev.devopsb72.store </app/schema/component.js  &>>/tmp/roboshop.log
+mongo --host mongodb-dev.devopsb72.store </app/schema/$component.js  &>>/tmp/roboshop.log

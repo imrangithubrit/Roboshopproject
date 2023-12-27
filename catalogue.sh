25ff5032aad1278006fe823a4d6e9e99c06993c5
imrangithubrit <devopsandaws2023@gmail.com>
2023-12-27 09:08:31 +0530

    as

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@ component=catalogue
 color="\e[36m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
+app_path="/app"
 
 
 echo -e "${color}  configuration node js ${nocolor}"
@@ -13,17 +14,17 @@ yum install nodejs -y   &>>log_file
 echo -e "${color} add Application user ${nocolor}"
 useradd roboshop   &>>log_file
 
-rm -rf /app
-mkdir /app 
+rm -rf ${app_path}
+mkdir ${app_path}
 
 echo -e "${color} Download application director ${nocolor}"
 
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>log_file
 
 echo -e "${color} Extract application content ${nocolor}"
-cd /app   &>>log_file
+cd ${app_path}   &>>log_file
 unzip /tmp/$component.zip  &>>log_file
-cd /app 
+cd ${app_path} 
 
 echo -e "${color} Install nodejs sependencies ${nocolor}"
 npm install &>>log_file

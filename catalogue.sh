a94874e2f9e1a00fac90fb11ea6031a2d70c3465
imrangithubrit <devopsandaws2023@gmail.com>
2023-12-28 23:38:06 +0530

    ds

--- catalogue.sh
+++ catalogue.sh
@@ -5,11 +5,5 @@ component=catalogue
 
 nodejs
 
-echo -e "${color} copy mongodb repo file ${nocolor}"
-#copy mpng.rep
-cp /project/Roboshopproject/mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
+mongo_schema_setup
 
-echo -e "${color} install mongodb ${nocolor}"
-yum install mongodb-org-shell -y   &>>${log_file}
-
-mongo --host mongodb-dev.devopsb72.store </app/schema/$component.js  &>>${log_file}
--- common.sh
+++ common.sh
@@ -39,3 +39,16 @@ nodejs()  {
   systemctl enable $component &>>${log_file}
   systemctl restart $component  &>>${log_file}
 }
+
+mongo_schema_setup()
+{
+echo -e "${color} copy mongodb repo file ${nocolor}"
+#copy mpng.rep
+cp /project/Roboshopproject/mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
+
+echo -e "${color} install mongodb ${nocolor}"
+yum install mongodb-org-shell -y   &>>${log_file}
+
+mongo --host mongodb-dev.devopsb72.store </app/schema/$component.js  &>>${log_file}
+
+}

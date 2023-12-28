1cce5140247d683878dafab0760231105dd6571b
imrangithubrit <devopsandaws2023@gmail.com>
2023-12-29 00:23:18 +0530

    sdl;

--- common.sh
+++ common.sh
@@ -3,52 +3,87 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
+app_presetup(){
+    echo -e "${color} add Application user ${nocolor}"
+    useradd roboshop   &>>${log_file}
+    rm -rf ${app_path} &>>${log_file}
+
+    echo -e "${color} add folder ${nocolor}"
+    mkdir ${app_path}  &>>${log_file}
+    
+    echo -e "${color} Download application director ${nocolor}"
+
+    curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>${log_file}
+
+    echo -e "${color} unzip the folder ${nocolor}"
+    cd ${app_path} &>>${log_file}
+    unzip /tmp/$component.zip &>>${log_file}
+}
+systemd_setup() {
+    echo -e "${color} copy the service file ${nocolor}"
+    cp /project/Roboshopproject/$component.service /etc/systemd/system/$component.service  &>>${log_file}
+
+    echo -e "${color} start the $component service ${nocolor}"
+
+    systemctl daemon-reload &>>${log_file}
+
+    systemctl enable $component &>>${log_file}
+    systemctl restart $component &>>${log_file}
+
+
+}
+mysql_schema_setup(){
+    echo -e "\e[33m install the mysql ${nocolor}"
+    yum install mysql -y &>>${log_file}
+
+
+    mysql -h mysql-dev.devopsb72.store	 -uroot -pRoboShop@1 < /app/schema/$component.sql &>>${log_file}
+
+}
 
 nodejs()  {
-  echo -e "${color}  configuration node js ${nocolor}"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log_file}
-    
-  echo -e "${color}  install nodejs ${nocolor}"
-  yum install nodejs -y   &>>${log_file}
+    echo -e "${color}  configuration node js ${nocolor}"
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log_file}
+        
+    echo -e "${color}  install nodejs ${nocolor}"
+    yum install nodejs -y   &>>${log_file}
 
-  echo -e "${color} add Application user ${nocolor}"
-  useradd roboshop   &>>${log_file}
+    app_presetup
 
-  rm -rf ${app_path}
-  mkdir ${app_path}
+    echo -e "${color} Install nodejs sependencies ${nocolor}"
+    npm install &>>${log_file}
 
-  echo -e "${color} Download application director ${nocolor}"
-
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>${log_file}
-
-  echo -e "${color} Extract application content ${nocolor}"
-  cd ${app_path}   &>>${log_file}
-  unzip /tmp/$component.zip  &>>${log_file}
-  cd ${app_path} 
-
-  echo -e "${color} Install nodejs sependencies ${nocolor}"
-  npm install &>>${log_file}
-
-  echo -e "${color} setup $component service ${nocolor}"
-    # cp service
-  cp /project/Roboshopproject/$component.service /etc/systemd/system/$component.service  &>>${log_file}
-
-  echo -e "${color}  start $component service ${nocolor}"
-  systemctl daemon-reload  &>>${log_file}
-
-  systemctl enable $component &>>${log_file}
-  systemctl restart $component  &>>${log_file}
+    systemd_setup
 }
 
 mongo_schema_setup()
 {
-echo -e "${color} copy mongodb repo file ${nocolor}"
-#copy mpng.rep
-cp /project/Roboshopproject/mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
+    echo -e "${color} copy mongodb repo file ${nocolor}"
+    #copy mpng.rep
+    cp /project/Roboshopproject/mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
 
-echo -e "${color} install mongodb ${nocolor}"
-yum install mongodb-org-shell -y   &>>${log_file}
+    echo -e "${color} install mongodb ${nocolor}"
+    yum install mongodb-org-shell -y   &>>${log_file}
 
-mongo --host mongodb-dev.devopsb72.store </app/schema/$component.js  &>>${log_file}
+    mongo --host mongodb-dev.devopsb72.store </app/schema/$component.js  &>>${log_file}
 
+}
+
+
+
+maven() {
+    echo -e "${color} Install manven ${nocolor}"
+    yum install maven -y  &>>${log_file}
+
+    app_presetup
+
+    echo -e "${color} move to target foler ${nocolor}"
+    cd ${app_path} &>>${log_file}
+    mvn clean package &>>$l{og_file}
+    mv target/$component-1.0.jar $component.jar &>>${log_file}
+
+   
+   mysql_schema_setup 
+    
+    systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -1,41 +1,5 @@
-echo -e "\e[33m Install manven \e[0m"
-yum install maven -y  &>>/tmp/roboshop.log
+source common.sh
+component=shipping
 
-echo -e "\e[33m add user \e[0m"
-useradd roboshop  &>>/tmp/roboshop.log
+maven
 
-rm -rf /app &>>/tmp/roboshop.log
-
-echo -e "\e[33m add folder \e[0m"
-mkdir /app  &>>/tmp/roboshop.log
-
-echo -e "\e[33m download the content \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33m unzip the folder \e[0m"
-cd /app &>>/tmp/roboshop.log
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33m move to target foler \e[0m"
-cd /app &>>/tmp/roboshop.log
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
-
-
-echo -e "\e[33m copy the service file \e[0m"
-cp /project/Roboshopproject/shipping.service /etc/systemd/system/shipping.service  &>>/tmp/roboshop.log
-
-echo -e "\e[33m start the shippping service \e[0m"
-
-systemctl daemon-reload &>>/tmp/roboshop.log
-
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl restart shipping &>>/tmp/roboshop.log
-
-echo -e "\e[33m install the mysql \e[0m"
-yum install mysql -y &>>/tmp/roboshop.log
-
-
-mysql -h mysql-dev.devopsb72.store	 -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
-
-systemctl restart shipping &>>/tmp/roboshop.log
